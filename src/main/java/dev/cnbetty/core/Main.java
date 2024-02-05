e14d7e01b0de92bcdc81f257b667c0b43087f312
Hinterhältiger <ole@conrady.info>
2024-02-05 16:58:02 +0100

    Added basic configuriation support via the paper YAML API

--- src/main/java/dev/cnbetty/core/Main.java
+++ src/main/java/dev/cnbetty/core/Main.java
@@ -1,21 +1,31 @@
 package dev.cnbetty.core;
 
-import dev.cnbetty.core.commands.CoreIdentify;
+import dev.cnbetty.core.commands.CommandRegistry;
 import dev.cnbetty.core.logger.Logger;
-import dev.cnbetty.core.commands.*;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.util.Objects;
 
 public class Main extends JavaPlugin {
     public static final String version = "0.0.1-DEV";
     public static final Logger logger = new Logger("core");
+    private static Main instance;
+
+    public static Main getPlugin() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
         logger.info("plugin loaded.");
 
         CommandRegistry.registerAll(this);
         logger.info("commands loaded.");
+
+        saveResource("config.yml", false);
+        logger.info("config initialized");
+
+        instance = this;
     }
+
     @Override
     public void onDisable() {
         // Plugin shutdown logic
--- src/main/java/dev/cnbetty/core/settings/SettingsFields.java
+++
@@ -1,5 +0,0 @@
-package dev.cnbetty.core.settings;
-
-class SettingsFields {
-
-}
