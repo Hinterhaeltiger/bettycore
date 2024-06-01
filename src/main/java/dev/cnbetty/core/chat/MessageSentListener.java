349386ddb967b1171459a8283cb15c2f9bd6ac1b
Hinterhältiger <ole@conrady.info>
2024-06-01 18:37:32 +0200

    updated chat message formatting

--- src/main/java/dev/cnbetty/core/chat/MessageSentListener.java
+++ src/main/java/dev/cnbetty/core/chat/MessageSentListener.java
@@ -4,6 +4,7 @@ import io.papermc.paper.chat.ChatRenderer;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,13 +13,14 @@ import org.jetbrains.annotations.NotNull;
 public class MessageSentListener implements Listener, ChatRenderer {
     @EventHandler
     public void onChat(AsyncChatEvent event) {
-        event.renderer(this); // Tell the event to use our renderer
+        event.renderer(this);
     }
 
     @Override
     public @NotNull Component render(@NotNull Player source, @NotNull Component sourceDisplayName, @NotNull Component message, @NotNull Audience viewer) {
-        return sourceDisplayName
-                .append(Component.text("> "))
-                .append(message);
+        return sourceDisplayName.color(TextColor.color(0x636363))
+                .append(Component.text(": \"", TextColor.color(0x636363)))
+                .append(message).color(TextColor.color(0x00face))
+                .append(Component.text("\"", TextColor.color(0x636363)));
     }
 }
