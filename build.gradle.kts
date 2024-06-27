45591458d9b3411ab451a816cec1bf7e7a08e9a1
Hinterhaeltiger <ole@conrady.info>
2024-06-27 14:13:56 +0200

    Added plugins for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,8 +31,9 @@ tasks {
     }
     runServer {
         downloadPlugins{
-            url("https://cdn.modrinth.com/data/evkiwA7V/versions/2aIoW39P/AxiomPaper-1.5.11.jar")
+            url("https://cdn.modrinth.com/data/evkiwA7V/versions/e4UvdAEk/AxiomPaper-1.5.12.jar")
             url("https://ci.lucko.me/job/spark/418/artifact/spark-bukkit/build/libs/spark-1.10.73-bukkit.jar")
+            url("https://cdn.modrinth.com/data/1u6JkXh5/versions/DBLNBwrB/worldedit-bukkit-7.3.4-beta-01.jar")
         }
         minecraftVersion("1.21")
     }
